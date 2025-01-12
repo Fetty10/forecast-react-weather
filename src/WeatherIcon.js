8e693163d8a38bf50f0e948228d020eff49973da
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-12 01:50:46 +0000

    Added forecast layout

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,8 +26,8 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#085ED6"
-      size={64}
+      color="#202124"
+      size={props.size}
       animate={true}
     />
   );
